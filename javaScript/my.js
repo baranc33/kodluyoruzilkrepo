36ffeb6115cabfb4450ee5ab237c60de33fea287
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-01 16:56:10 +0300

    quest

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,7 +1,19 @@
 
-// ternary operatörü
+//kosullarla calismak egzersiz
+/*
+1- prompt ile aldigin bilgiye göre assagidaki yapiya kulanarak notun bilgisni infoda yazdır
+AA  90-100
+BA  85 89
+BB  80 84
+CB  75-79
+CC  70-74
+DC  65-69
+DD  60-64
+FD  50-59
+FF  00-49
 
-let info=document.querySelector("#info");
- let userName=prompt('Adınızı Giriniz');
+2- KLLANICININ GİRDİĞİ VERİNİN İSTEDİĞİMİZ aralıkta olup olmadığını kotnrol edin
+3- ff bilgisinde üzgün surat ikonu çıkart, diğerlerinde gülücük
+4-ff class bilgisi text-danger,diğerleri ise text-primary olsun
 
- info.innerHTML= `${userName.length>0 ? userName:'Kullanıcı bulunamadı!'}`;d
+*/
