fec6f52c6d0773fdd193cc4b7bd9651962610f6f
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-02 17:23:37 +0300

    fucntionhello

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,7 +1,7 @@
-var money;
-var canBuy = 
-    (money < 17) ? "Satın alamazsın..":
-    (money > 30) ? "Satın alabilirsin..":
-    "Para miktarını girmen gerekmektedir..";
 
-console.log(canBuy) 
+function hello(){
+    console.log('Hello');
+}
+
+
+hello();
