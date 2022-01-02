7d42b3b37f5093d8eed8c58a5566ad0a75e7228f
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-02 17:42:39 +0300

    default parameter

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,9 +1,11 @@
 let firstName="Hakan";
-function Selamlama(name){
+function Selamlama(name="insan"){
 return `Selam ${name} `;
 }
 
 
 let Selam=Selamlama(firstName);
+let Selam2=Selamlama();
 
-console.log(Selam);
+console.log(Selam);// selam hakan
+console.log(Selam2);//selam insan
