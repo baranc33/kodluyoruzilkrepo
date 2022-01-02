0fcddab638e2fe1e7c26f0444b9eaa494fc9e574
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-02 17:22:55 +0300

    quest

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,67 +1,7 @@
+var money;
+var canBuy = 
+    (money < 17) ? "Satın alamazsın..":
+    (money > 30) ? "Satın alabilirsin..":
+    "Para miktarını girmen gerekmektedir..";
 
-//kosullarla calismak egzersiz
-/*
-1- prompt ile aldigin bilgiye göre assagidaki yapiya kulanarak notun bilgisni infoda yazdır
-AA  90-100
-BA  85 89
-BB  80 84
-CB  75-79
-CC  70-74
-DC  65-69
-DD  60-64
-FD  50-59
-FF  00-49
-
-2- KLLANICININ GİRDİĞİ VERİNİN İSTEDİĞİMİZ aralıkta olup olmadığını kotnrol edin
-3- ff bilgisinde üzgün surat ikonu çıkart, diğerlerinde gülücük
-4-ff class bilgisi text-danger,diğerleri ise text-primary olsun
-
-*/
-const SMILE=`:)  `;
-let info=document.querySelector('#info');
-
-let examGraade=prompt("puanı girin");
-let textInfo; 
-if(examGraade>0&&examGraade<100){
-    textInfo=SMILE;
-    info.style.color='blue';
-
-    if(examGraade>=90){
-        textInfo +='AA';
-        }
-        else if(examGraade>=85){
-            textInfo+='BA';
-        }
-        else if(examGraade>=80){
-            textInfo+='BB';
-        }
-        else if(examGraade>=75){
-            textInfo+='CB';
-        }
-        else if(examGraade>=70){
-            textInfo+='CC';
-        }
-        else if(examGraade>=65){
-            textInfo+='DC';
-        }
-        else if(examGraade>=60){
-            textInfo+='DD';
-        }
-        else if(examGraade>=50){
-            textInfo +='FD';
-        }
-        else {
-            textInfo =':(  50';
-            info.style.color='red';
-        }
-        
-}
-else{
-    textInfo='Hatalı giriş';
-}
-
-
-
-
-info.innerHTML=`${textInfo} -> ${examGraade}`;
-console.log(textInfo);
+console.log(canBuy) 
