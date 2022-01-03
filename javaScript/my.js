5d74461512d82978b6f4b6726c4d9f327c4bbf79
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-03 19:47:36 +0300

    mouseover

--- javaScript/my.js
+++ javaScript/my.js
@@ -2,19 +2,8 @@
 //https://www.w3schools.com/jsref/dom_obj_event.asp
 
 let greeting=document.querySelector('#info');
-// greeting.addEventListener('click', function(){
-// // bu şekilde direk fonksyon yazılabilir yada aşşağıdaki gibi
-// // dışardan belirttiğimiz bir fonksyon yolliyabiliriz
-// });
+greeting.addEventListener("mouseover",domClick);
 
-
-greeting.addEventListener("click",domClick);
-// function domClick(){
-//     console.log(this.innerHtml="Tıklandığında değişti");
-//     this.style.color='red';
-// }
-
-// ternar ile bi dğeişiklik yapalım
 function domClick(){
     this.style.color=='red'? this.style.color='blue':this.style.color='red';
 }
