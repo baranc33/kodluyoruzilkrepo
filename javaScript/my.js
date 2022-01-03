9ec05bccdc58a47c8d112a71dec1e351a3533f2a
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-03 19:37:25 +0300

    ret

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,14 +1,24 @@
 // önce normal function tanımlayalım ardından bunu 
 // arrow ile tanımlayalım
-function hello(firstName){
-console.log(`Merhaba ${firstName}`)
+function hello(firstName) {
+    console.log(`Merhaba ${firstName}`)
 }
 hello('javaScript')
 
-const helloFuncV1=(firstName) =>{console.log(`Merhaba ${firstName}`)}
+const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`) }
 helloFuncV1('Hakan');
-const helloFuncV2=firstName=>{console.log(`Merhaba ${firstName}`)}
+const helloFuncV2 = firstName => { console.log(`Merhaba ${firstName}`) }
 helloFuncV2('Baran');
 
-const helloFuncV3=(firstName,lastName) =>{console.log(`Merhaba ${firstName} ${lastName}`)}
-helloFuncV3('Hakan','Çakır');
+const helloFuncV3 = (firstName, lastName) => { console.log(`Merhaba ${firstName} ${lastName}`) }
+helloFuncV3('Hakan', 'Çakır');
+
+
+// return işlemide
+const helloFuncV4 = (firstName, lastName) => {
+    return`Merhaba ${firstName} ${lastName}`;
+}
+
+let a= helloFuncV4('hakan baran','çakır');
+
+console.log(a);
