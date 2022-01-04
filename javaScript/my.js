a4d35b13ef414dee6d23e7b09e941420909ed125
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-04 20:12:39 +0300

    onclick

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,17 +1,6 @@
-// bi
+let yellowball = document.getElementById("yellow");
+let purpleBall = document.getElementById("purple");
 
-let counter=0;
-let counterDom=document.querySelector('#counter');
-let increaseDom=document.querySelector('#increase');
-let decreaseDom=document.querySelector('#decrease');
-
-
-counterDom.innerHTML=counter;
-
-increaseDom.addEventListener('click',clickEvent);
-decreaseDom.addEventListener('click',clickEvent);
-
-function clickEvent() {
-    this.id=='increase'?++counter:--counter;
-    counterDom.innerHTML=counter;
+yellowball.onclick = function() {
+purpleBall.style.backgroundColor = "yellow";
 }
