c53de5275b3f3a015892102b68bfc2ba448ba50b
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-05 20:19:04 +0300

    change

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,10 +1,10 @@
-//id=fname olan nesne çağrılarak keyboard değişkenine atandı
-const keyboard =document.querySelector('#fname');
-//keyboard isimli değişkene "cut" etkinliği atandı ve fonksiyon ismi parametre olarak atandı
-keyboard.addEventListener("cut",cuttingFunction);
-//cuttingFunction isimli fonksiyon ile form inputuna yazılan girdileri klavye ctrl+c ile kesildiğinde etkinlik çalışmaktadır
-function cuttingFunction(event){
-  console.log("etkinlik tipi: " , event.type)
-  } 
+//id=select olan nesne çağrılarak color değişkenine atandı
+const color = document.querySelector("#select");
+//color değişkenine change etkinliği etkinliği atandı ve fonksiyon ismi parametre olarak atandı
+color.addEventListener('change' ,selectBox);
+//selectBox isimli fonksiyon ile selectbox her seçildiğinde etkinlik tipi =change ve değeri consola yazılmaktadır 
+function selectBox(event){
+    console.log("Etkinlik tipi : " + event.type);
+    console.log("deger : " + event.target.value);
+    }
   
-//console ekranı : etkinlik tipi: cut
