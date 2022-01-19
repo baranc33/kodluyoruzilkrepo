1468512b37c5b819df264f1691682adee66ad350
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-20 00:19:54 +0300

    removeitem

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,5 +1,12 @@
+//ekleme
 localStorage.setItem('key','value');
-
+// çağırma
 let a=localStorage.getItem('key');
 
 console.log(a);
+
+localStorage.removeItem('key');
+
+
+console.log('---')
+console.log(localStorage.getItem('key'));
