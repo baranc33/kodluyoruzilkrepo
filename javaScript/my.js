c770d933b4e99e3b7b349d07eca6faa1dabf4d2b
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-20 00:26:19 +0300

    2

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,12 +1,11 @@
-//ekleme
-localStorage.setItem('key','value');
-// çağırma
-let a=localStorage.getItem('key');
-
-console.log(a);
-
-localStorage.removeItem('key');
+let user={userName :"Hakan Baran Çakır", isActive :true};
 
 
-console.log('---')
-console.log(localStorage.getItem('key'));
+//obje olarak kaydetmek yerine json tipine çevirip atmalıyız
+localStorage.setItem('userInfo',JSON.stringify(user));
+
+let info=localStorage.getItem('userInfo');
+// bu bilgiyi parshe etmemiz lazım yoksa string olarka geliyor
+info =JSON.parse(info);
+console.log(info.userName);
+console.log(info.isActive);
