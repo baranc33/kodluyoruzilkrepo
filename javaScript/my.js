e307e511b9df6cb3406258abf7541538407c7b31
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-20 00:46:49 +0300

    submitcancel

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,14 +1,11 @@
-// sayı varsa sayı sayı yoksa 0/ couterı number yapmalıyız
-let counter= localStorage.getItem('counter')? Number(localStorage.getItem('counter')): 0;
-let counterDom=document.querySelector('#counter');
-let increaseDom=document.querySelector('#increase');
-let decreaseDom=document.querySelector('#decrease');
+//form submiti
+let formDom=document.querySelector('#userForm');
+formDom.addEventListener('submit',formSubmit);
 
-counterDom.innerHTML=counter;
-increaseDom.addEventListener('click',clickEvent);
-decreaseDom.addEventListener('click',clickEvent);
-function clickEvent() {
-    this.id=='increase'?++counter:--counter;
-    localStorage.setItem('counter',counter);
-    counterDom.innerHTML=counter;
+function formSubmit(event){// burda eventle yukarda belirttiğimiz işlemi
+    event.preventDefault();// submit işlemini iptal ettik
+
+    console.log('işlem gerçekleşti');
 }
+
+
