35b86a4fc2505ba3ef7531447b0ddb9327c4cdaf
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-21 19:49:08 +0300

    Array Function

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,35 +1,24 @@
-// array olusturmak
-let domain="hakanbarancakir.cf";
-let isActive=false;
-let items=[ 1, 2, 3,isActive,domain];
-//  Sona bilgi eklemek
-items.push(50);
-// başa ekleme
-items.unshift('baş');
+// çok kullanilan Array(dizi) metotları ve array icinde array
 
-console.log(items)
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+//https://www.w3schools.com/js/js_array_methods.asp
 
+let items=[1,2,3,4,5]
 
-// sondaki bilgiyi çıkarma
-items.pop()
-console.log(items)
-
-// bu bilgiyi yaklayada biliriz
-let sonitem=items.pop();
-console.log(sonitem)
-
-// ilk eleman almak
-let firstItemi=items.shift();
-console.log(firstItemi, '// Kalan dizi =',items);
-
-// dizideki elamnı değiştirmek direk değer atabiliriz işlemde yapabilirz
-items[0]=items[0]+2;
-
-console.log(items);
+//array içinde array
+let femaleUser=['Ayse','Hülya','Merve']
+let maleUsers=['Ahmet','Hakan','Mehmet']
 
 
+// Array icerisinde oge ayirmak -> splice(pos,item?)
+// array içerisindeki ogenin index bilgisini bulmak -> indexOf('value')
 
 
+// array kopyalamak -> slice,[...ES6]
+
+// array içersindeki bilgiyi stringe çevirmek  -> toString, join
+
+// iki array ilgisini birleştirmek -> [...ES6 ,  ...ES6]
 
 
-
+// istediğimiz index bilgisine öge eklemek --> splice (index,0,value)
