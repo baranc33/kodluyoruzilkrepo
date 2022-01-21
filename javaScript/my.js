6a3b18dcce23d069beb51f02dea71c1c395617b9
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-21 18:14:10 +0300

    list

--- javaScript/my.js
+++ javaScript/my.js
@@ -3,19 +3,25 @@ userFormDom.addEventListener('submit',formHandler);
 
 function formHandler(event){
     event.preventDefault();
-    const USER_Name=document.querySelector('#username');
-    const SCORE=document.querySelector('#score');
+    const USER_Name=document.querySelector('#username');// bilgi alacağımız input
+    const SCORE=document.querySelector('#score');//bilgi alacağımız input2
+    addItem(USER_Name.value,SCORE.value);
 }
 
 /*
- <li class="list-group-item d-flex justify-content-between align-items-center">
+ <li class="">
 A list item
-<span class="badge bg-primary rounded-pill">14</span>
 </li>
 */
 
-let userListDom=document.querySelector('#userList');
-function addItem(username,score){
-    let li
+let userListDom=document.querySelector('#userList');//içine ekliyeceğimiz ul
+// normal function yerine arrow function ile ekliyelim
+const  addItem =(username,score) =>{
+    let liDom=document.createElement('li')
+    liDom.innerHTML=`${username}
+<span class="badge bg-primary rounded-pill"> ${score}</span>`;// ekliceğimiz bilgi
+    // li nin classlarını ekliyelim
+    liDom.classList.add('list-group-item','d-flex' ,'justify-content-between', 'align-items-center')
+    userListDom.append(liDom);// ekleme işlemi
 }
 
