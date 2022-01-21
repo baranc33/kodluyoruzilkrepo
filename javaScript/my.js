6e79bbd4218a20c42dd442374ac75177c8b1ea87
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-21 18:43:27 +0300

    array

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,14 +1,23 @@
 // array olusturmak
 let domain="hakanbarancakir.cf";
 let isActive=false;
-let items=[1,2,3,isActive,domain];
+let items=[ 1, 2, 3,isActive,domain];
 
 console.log(items);
 
+// eleman ekleme 1 . daha farklı şekillerdede ekleme yapabiliriz 
+// detayları ilerde görcez
+items[items.length]="deneme";
+console.log(items);
+
+let emptyArray=[]// boş liste
 
 // array içersindeki eleman/og sayisini ögrenmek
+console.log(items.length);
 
 //ilk elemanın cağrılması
+console.log(items[0]);
 // son elemanın  çağrılması
+console.log(items[items.length-1]);
 
 // degisken iindeki bilginin array olup olmadığını kontrol etmek
