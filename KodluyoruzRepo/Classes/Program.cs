695e31cb6525304ca55a91e4946d69fdba2423ae
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-23 13:37:18 +0300

    class

--- KodluyoruzRepo/Classes/Program.cs
+++ KodluyoruzRepo/Classes/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// sınıflar nesne oluşturmak için kullandığımız yaplar
+
+
+Calisan calisan1 = new Calisan()
+{
+    Departman = "Yazılım",
+    No = 1,
+    SoyAd = "Çakır",
+    ad = "Hakan Baran"
+};
+
+calisan1.calisanBilgileri();
+
+
+// söz dizimi
+class SinifAdi
+{
+    /*
+       property
+      [Erişim Belirleyici] [Veri Tipi] ÖzellikAdi;
+
+      Metot
+      [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi(paramterle listesi)
+      {
+      }
+     */
+
+
+    // erişim belirleyiciler
+    /*
+     public heryerden erişilebilir
+     private= sadece scopei çinden erişilebilir
+    internal = sadece proje içinden
+    protected= sadece tanımlandığı veya kalıtım alan sınıflarda erişileilibir
+     
+     */
+}
+
+
+
+class Calisan
+{
+    public string ad;
+    public string SoyAd;
+    public int No;
+    public string Departman;
+
+
+    public void calisanBilgileri()
+    {
+        Console.WriteLine("Çalışan Adı:{0}",ad);
+        Console.WriteLine("Çalışan Soy Adı:{0}",SoyAd);
+        Console.WriteLine("Çalışan Numarası:{0}",No);
+        Console.WriteLine("Çalışan Departmanı :{0}",Departman);
+    }
+}
