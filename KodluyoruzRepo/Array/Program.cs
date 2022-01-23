bdd6405cdaef5332ad0fa985d0638751d84fcd61
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-23 13:28:48 +0300

    array2

--- KodluyoruzRepo/Array/Program.cs
+++ KodluyoruzRepo/Array/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿  int[] sayiDizisi = { 11, 2, 23, 5, 6 };
+
+            Console.WriteLine("*********** Sırasız Dizi*********");
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+
+            Console.WriteLine("*********** Sıralı Dizi*********");
+
+            Array.Sort(sayiDizisi);
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+
+
+            Console.WriteLine("*********** Array Clear*********");
+
+            Array.Clear(sayiDizisi, 2, 2);
+
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+
+
+            Console.WriteLine("*********** Array Reverse *********");
+
+            Array.Reverse(sayiDizisi);
+
+            foreach (var item in sayiDizisi)
+                Console.WriteLine(item);
+
+
+            Console.WriteLine("*********** Array IndexOf *********");
+
+            int ads = Array.IndexOf(sayiDizisi,2);
+
+            Console.WriteLine(ads);
+
+
+            Console.WriteLine("*********** Array Resize*********");
+
+            Array.Resize<int>(ref sayiDizisi, 9);
