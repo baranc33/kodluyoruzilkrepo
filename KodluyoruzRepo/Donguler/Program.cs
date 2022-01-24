ee72dbe637127a4b7793750fc2e1764a7cdfd7d6
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-24 13:40:51 +0300

    Donguler

--- KodluyoruzRepo/Donguler/Program.cs
+++ KodluyoruzRepo/Donguler/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+            Console.WriteLine("Lütfen Bir sayı giriniz");
+            int sayac = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            int tektoplam = new int();
+            int çiftToplam = new int();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    tektoplam += i;
+                }
+                else
+                {
+                    çiftToplam += i;
+                }
+
+            }
+
+
+            Console.WriteLine("tek" +tektoplam+" çift = "+çiftToplam);
+
+
