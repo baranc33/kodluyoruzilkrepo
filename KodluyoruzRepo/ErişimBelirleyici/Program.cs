f7f8c7c44c2224f84e999386a4d639eb5feeec5d
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-24 13:41:21 +0300

    AccessModifier

--- KodluyoruzRepo/ErişimBelirleyici/Program.cs
+++ KodluyoruzRepo/ErişimBelirleyici/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Calisan calisan=new Calisan("Hakan Baran","Çakır",1,"Yazılım");
+calisan.calisanBilgileri();
+
+class Calisan
+{
+    public string ad;
+    public string SoyAd;
+    public int No;
+    public string Departman;
+
+    public Calisan()
+    {
+
+    }
+            
+    // constructer  kurucu metot
+    public Calisan(string ad, string soyad, int no, string departman)
+    {
+        this.ad = ad;
+        this.SoyAd = soyad;
+        this.No = no;
+        this.Departman = departman;
+    }
+    public void calisanBilgileri()
+    {
+        Console.WriteLine("Çalışan Adı:{0}", ad);
+        Console.WriteLine("Çalışan Soy Adı:{0}", SoyAd);
+        Console.WriteLine("Çalışan Numarası:{0}", No);
+        Console.WriteLine("Çalışan Departmanı :{0}", Departman);
+    }
+}
