c30e259d25e6283e4779f4bd0a89d8d355433cf9
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-25 13:46:29 +0300

    metot

--- KodluyoruzRepo/Metotlar/Program.cs
+++ KodluyoruzRepo/Metotlar/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+ErişimBelirteci GeriDönüştipi MetotAdi(parametreler)
+{
+Kodlarımız;
+}
+
+ */
+
+int Topla(int deger1, int deger2)
+{
+    return deger1 + deger2;
+}
+
+
+
+int a = 2;
+int b = 3;
+
+string cevap = Topla(a, b).ToString();
+Metotlar.EkranaYazdir(cevap);
+
+
+class Metotlar
+{
+    static public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    static int arttırVeTopla(int deger1, int deger2)
+    {
+        deger1++;
+        deger2++;
+
+        return deger1 + deger2;
+    }
+}
