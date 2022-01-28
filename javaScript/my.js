3a5c720ded82e8614241fac979c1fdab84b0d22b
Hakan Baran Çakir <baranc3363@gmail.com>
2022-01-28 21:09:35 +0300

    quest

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,13 +1,12 @@
-// foreach
+// Array filter kullanımı
 
+// 5 harften fazla olanları yeni liste eklicez??
+ 
+const PRODUCTS=["Mic","Cable","Speaker","Desktop PC","Server","Mouse","Keyboard"]
 
-const PRODUCT=['Leptop','phone','speaker','desktop pc']
-
-
-
-PRODUCT.forEach((product,index,array)=> 
-{
-    // bütün kelimeleri büyük harfe çevirelims
-    console.log(array[index]=product.toUpperCase())
-})
-
+const USERS=[
+{fullname:"Ayse Sumer",isActive:false},
+{fullname:"Ahmet Urga",isActive:true},
+{fullname:"Asya Basar",isActive:true},
+{fullname:"Aksel Durmaz",isActive:true}
+]
