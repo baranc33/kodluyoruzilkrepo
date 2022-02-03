3b37ce7a3e62a580db950171060a6907dd903d39
Hakan Baran Çakir <baranc3363@gmail.com>
2022-02-03 20:45:09 +0300

    continue

--- javaScript/my.js
+++ javaScript/my.js
@@ -4,8 +4,8 @@ let counter=0;
 
 for(counter;counter<5;counter++){
 
-    if(LOREM_LIST[counter]=='test'){
-        break;
+    if(LOREM_LIST[counter]!='test'){
+        continue;
     }
     console.log(LOREM_LIST[counter])
 }
