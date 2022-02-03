0454a3cf763f5231fafc746f6b7919354a204f71
Hakan Baran Çakir <baranc3363@gmail.com>
2022-02-03 20:40:41 +0300

    ul li for

--- javaScript/my.js
+++ javaScript/my.js
@@ -3,10 +3,12 @@
 
 let user =['hakan','baran','çakır']
 
-
+let userlistDom=document.querySelector('#userList')
 
 for(let index=0; index<user.length; index++)
 {
+    const liDom=document.createElement('li')
+    liDom.innerHTML=user[index];
+    userlistDom.appendChild(liDom);
 
-    console.log(user[index]);
 }
