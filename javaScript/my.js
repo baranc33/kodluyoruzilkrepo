5901fb6ed1e73440606dabe2a02c0131ae1cc6e7
Hakan Baran Çakir <baranc3363@gmail.com>
2022-02-04 20:52:04 +0300

    while

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,11 +1,8 @@
-const LOREM_LIST=["hakan","baran","çakır","test"]
+// While
 
 let counter=0;
 
-for(counter;counter<5;counter++){
-
-    if(LOREM_LIST[counter]!='test'){
-        continue;
-    }
-    console.log(LOREM_LIST[counter])
+while(counter<10)
+{
+    console.log(counter++);
 }
