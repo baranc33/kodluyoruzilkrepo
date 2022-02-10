79c87f29cc3f8c6bcaf7b25fc9100b14499b0ef3
Hakan Baran Çakir <baranc3363@gmail.com>
2022-02-10 21:19:05 +0300

    map1

--- javaScript/my.js
+++ javaScript/my.js
@@ -9,3 +9,9 @@ const USERS=["AYSE","MehMet","TugCE",'AySEL']
 // username içinde orjinal isim kalsın
 // shortname içinde ilk harf büyük .(A.)
 // newname içinde ilk harf büyük olsun
+
+
+const NEW_USERS=USERS.map(user=>user.toLocaleLowerCase())
+
+console.log(USERS)
+console.log(NEW_USERS)
