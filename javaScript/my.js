38b61a1bbdd6263a9b0a062ad3dc84f453a79697
Hakan Baran Çakir <baranc3363@gmail.com>
2022-02-10 21:18:20 +0300

    quest

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,23 +1,11 @@
-// Array filter kullanımı
-
-// 5 harften fazla olanları yeni liste eklicez??
- 
-const PRODUCTS=["Mic","Cable","Speaker","Desktop PC","Server","Mouse","Keyboard"]
-
+// array map kullanımı
+// foreache benzerdir tek farkı yeni bir liste
+// elde etmemizii sağlar
+const USERS=["AYSE","MehMet","TugCE",'AySEL']
 
 
-const NEW_PRODUCTS=PRODUCTS.filter(product=>product.length>5)
-// filtreledeki ve karakter uzunluğu 5 ten büyük olanları listeye attık
 
-console.log(NEW_PRODUCTS)
-    
 
-const USERS=[
-{fullname:"Ayse Sumer",isActive:false},
-{fullname:"Ahmet Urga",isActive:true},
-{fullname:"Asya Basar",isActive:true},
-{fullname:"Aksel Durmaz",isActive:true}
-]
-const AKTIVE_USER=USERS.filter(user =>user.isActive==true)
-
-console.log(AKTIVE_USER)
+// username içinde orjinal isim kalsın
+// shortname içinde ilk harf büyük .(A.)
+// newname içinde ilk harf büyük olsun
