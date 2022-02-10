9504356bd6740b5fc49586279e7946f6c4416ca1
Hakan Baran Çakir <baranc3363@gmail.com>
2022-02-10 21:26:22 +0300

    refactoring

--- javaScript/my.js
+++ javaScript/my.js
@@ -14,11 +14,17 @@ const USERS=["AYSE","MehMet","TugCE",'AySEL']
 const NEW_USERS=USERS.map(user=>user.toLocaleLowerCase())
 
 console.log(NEW_USERS)
-// nesnemizin formatını değiştirelim
-const USERS_OBJ=USERS.map(item=>
-    {// ilk harfler büyük diğerleri küçük
-        return {userName:item,shortName:`${item[0]}.`,newName:`${item[0].toUpperCase()}${item.slice(1).toLocaleLowerCase()}`}
-    }
-    )
+// // nesnemizin formatını değiştirelim
+// const USERS_OBJ=USERS.map(item=>
+//     {// ilk harfler büyük diğerleri küçük
+//         return {userName:item,shortName:`${item[0]}.`,newName:`${item[0].toUpperCase()}${item.slice(1).toLocaleLowerCase()}`}
+//     }
+//     )
+
+// yukardakinin daha temiz halini yazalım
+
+const USERS_OBJ=USER.map(item=>(
+   {userName:item,shortName:`${item[0]}.`,newName:`${item[0].toUpperCase()}${item.slice(1).toLocaleLowerCase()}`}
+))
 
     console.log(USERS_OBJ)
