b7a00d849a37755c0d507d8fc0f20475b6e63d9f
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-01 14:57:27 +0300

    karşılaştırma

---
+++ javaScript/my.js
@@ -0,0 +1,12 @@
+// karşılattırma operatörleri
+// == değer karşılaştırır
+//==== tür ve değer karşılaştırır.
+// negatif şekilleri  !=  !==
+let price="100";
+
+
+console.log(price==100);// true
+console.log(price === 100);//false
+
+
+
