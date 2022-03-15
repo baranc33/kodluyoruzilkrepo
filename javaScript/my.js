5292065cb2b38b6116eeedfc9daf2930e914c714
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-15 23:07:10 +0300

    dom

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,24 +1,12 @@
 
 
-let name ="hakan";
+console.log(document.links);// link bilgisi
+console.log(document.links.namedItem);// link bilgisi
+console.log(document.location);// location
+console.log(document.location.pathname);// path
+console.log(document.body);// body,header gibi kodları alabiliriz
 
-// bag tikler alt-gr+ virgül normal  tırnak işareti dğeil
-let info=` ${name[4]}`; // indexer da kullanabiliriz
 
-console.log("Length" ,name.length);// karakter uzunluğu
-console.log(name[0]);// indexer
-console.log(name.charAt(0));// indexir
-console.log("Büyük Harf ",name.toUpperCase());// 
-console.log("Küçük Harf ",name.toLowerCase());// 
-console.log("index of = ",name.indexOf('k'));// 
-console.log("index Arama ",name.search("an"));// kelimenin başladığı indexi verir
-// eğer yoksa -1 döner
-console.log("Belli bir kısım",name.slice(0,2));//  ha
-console.log("overloading",name.slice(1));// akan
 
-console.log("replace : ",name.replace('a','e'));// hekan
-console.log("",name.includes('a'));//varmı diye sorar true false döner 
-console.log("endWith :",name.endsWith('n'));// sonu kontrol
-console.log("",name);// 
-console.log("",name);// 
-console.log("",name);// 
+console.log("url",document.URL);
+console.log("baseURI :",document.baseURI);
