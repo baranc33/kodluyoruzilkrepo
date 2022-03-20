c6576d76fa7b78fef610f694c69339eaacd2e2f6
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-21 00:56:16 +0300

    last

--- javaScript/my.js
+++ javaScript/my.js
@@ -3,7 +3,7 @@ let formDom=document.querySelector('#userForm');
 formDom.addEventListener('submit',formSubmit);
 
 function formSubmit(event){// burda eventle yukarda belirttiğimiz işlemi
-    event.preventDefault();// submit işlemini iptal ettik
+    // event.preventDefault();// submit işlemini iptal ettik
     let scoreInputDom=document.querySelector('#score');
 // input içindeki value değerini alalım
    localStorage.setItem('score',scoreInputDom.value);
