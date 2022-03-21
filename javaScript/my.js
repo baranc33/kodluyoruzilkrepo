ceeb3dcb6dcfdad8f0804a275905240436702027
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-21 22:53:33 +0300

    object copy

--- javaScript/my.js
+++ javaScript/my.js
@@ -5,19 +5,31 @@ let setting={
     ip:"127.0.0.1",
     serverName:"Kodluyoruz.org"
 }
-//obje içersindeki nesneyi alabiliriz
-// let userName=setting.userName
+// // obje içersindeki nesneyi alabiliriz
+// // let userName=setting.userName
 
 // let {userName,password,isActive,ip,serverName}=setting;
-// yukardaki yöntemle hepsini birer değişken yaptık
+// // yukardaki yöntemle hepsini birer değişken yaptık
 
 // console.log(userName);
 // console.log(password,isActive);
 // console.log(ip,serverName);
 
-//istersek bunlarda değişken ismini değiştirebilirz
-//ip yerine server ip yapalım
+// // istersek bunlarda değişken ismini değiştirebilirz
+// // ip yerine server ip yapalım
 
-let {userName,password,isActive,ip:ServerIp,serverName}=setting;
+// let {userName,password,isActive,ip:ServerIp,serverName}=setting;
 
-console.log(ServerIp)
+// console.log(ServerIp)
+// Bazı kısımlarını alalım geri kalan kısımlarını bir değişkene array olarak atalım
+// let {userName:userName2,password:password2,isActive:isActive2,...newSettings}=setting
+
+// console.log(userName2)
+// console.log(newSettings)
+
+
+// objenin destructring ile kopyalama
+let {...setting2}=setting
+setting.userName="Değişti"
+console.log(setting)
+console.log(setting2)
