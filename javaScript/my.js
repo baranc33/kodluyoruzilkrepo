b67c72b47259654bd8a39a528a61fc8af81256e5
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-21 22:47:49 +0300

    destructring

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,10 +1,23 @@
-var birey = {
-	isim: 'Ali',
-	soyisim: 'Veli',
-	dogumYili: 1989,
-	merhabaDe: (age) => `Merhaba, ben ${birey.isim} ${birey.soyisim}, ${age} yaşındayım`,
-};
-birey.yasHesapla = function () {
-	return 2021 - this.dogumYili;
-};
-console.log(birey.merhabaDe(birey.yasHesapla()));
+let setting={
+    userName:"loremIpsum",
+    password:"BadPassword",
+    isActive:false,
+    ip:"127.0.0.1",
+    serverName:"Kodluyoruz.org"
+}
+//obje içersindeki nesneyi alabiliriz
+// let userName=setting.userName
+
+// let {userName,password,isActive,ip,serverName}=setting;
+// yukardaki yöntemle hepsini birer değişken yaptık
+
+// console.log(userName);
+// console.log(password,isActive);
+// console.log(ip,serverName);
+
+//istersek bunlarda değişken ismini değiştirebilirz
+//ip yerine server ip yapalım
+
+let {userName,password,isActive,ip:ServerIp,serverName}=setting;
+
+console.log(ServerIp)
