011e63ecefbd4dea1ab1498c8588464c459c914d
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-21 22:37:57 +0300

    object function

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,32 +1,19 @@
-let leptop1 ={
-    brand:'Apple',
-    model:"MacBook Pro",
-    // keyler sayı ile başliyamaz fakat bunu ezebiliriz.
-    "2kg":2// bu sekilde başa sayı ekleyebiliriz
+let user1={
+    firstName:"Bilgin",
+    lastName:"Uzman",
+    score: [1, 2, 3, 4],
+    isActive:true,
+    // fonksyon tanımlama yapılabilir
+    FullName:function(){
+        return `${this.firstName} ${this.lastName}`
+    },
+    shortName:function(){
+        return `${this.firstName[0].toUpperCase()}. ${this.lastName}`
+    },
+
 }
 
-console.log(leptop1.brand)
-console.log(leptop1["brand"])// alternatif kullanımı
-console.log(leptop1.model)
-console.log(leptop1["2kg"])// çağırmak için bu sekilde kullanılır
 
-//veri değiştirmek
-leptop1.brand="MAc"
-console.log(leptop1)
-leptop1["brand"]="Mac"
-console.log(leptop1)
-
-
-keys=Object.keys(leptop1)
-console.log(keys)// sadece keysleri verir
-// key yerine sadece valuelerde alınabilir
-values=Object.values(leptop1)
-
-
-// bu key bilgilerini foreachle  kullanabiliriz
-
-keys.forEach(item => {
-    console.log(item)
-    console.log(leptop1[item])
-});
-
+console.log(user1.score)
+console.log(user1.FullName())
+console.log(user1.shortName())
