02d2e84c6e9aa7d6d29e9d72662629dca209cd47
Hakan Baran Çakir <baranc3363@gmail.com>
2022-03-21 22:40:22 +0300

    arrow funcition

--- javaScript/my.js
+++ javaScript/my.js
@@ -1,19 +1,10 @@
-let user1={
-    firstName:"Bilgin",
-    lastName:"Uzman",
-    score: [1, 2, 3, 4],
-    isActive:true,
-    // fonksyon tanımlama yapılabilir
-    FullName:function(){
-        return `${this.firstName} ${this.lastName}`
-    },
-    shortName:function(){
-        return `${this.firstName[0].toUpperCase()}. ${this.lastName}`
-    },
-
-}
-
-
-console.log(user1.score)
-console.log(user1.FullName())
-console.log(user1.shortName())
+var birey = {
+	isim: 'Ali',
+	soyisim: 'Veli',
+	dogumYili: 1989,
+	merhabaDe: (age) => `Merhaba, ben ${birey.isim} ${birey.soyisim}, ${age} yaşındayım`,
+};
+birey.yasHesapla = function () {
+	return 2021 - this.dogumYili;
+};
+console.log(birey.merhabaDe(birey.yasHesapla()));
